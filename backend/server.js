c630632c048f4160902f0b8c7ae2b47557db5762
Yuvraj Soni <yuvrajsoni2k1@gmail.com>
2025-03-30 19:27:45 +0530

    feat:removed node modules

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
